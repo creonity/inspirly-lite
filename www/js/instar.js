5c6169699f1f9e15466383be9b8d9989d2980663
creonity <lukas.nagel@gmx.ch>
2017-08-13 01:03:51 +0200

    back

--- www/js/instar.js
+++ www/js/instar.js
@@ -65,67 +65,7 @@ AdMob.hideBanner();
 })
 
 
-
-
-
-
-
-
-
-
-
-
-function Device() {
-    this.available = false;
-    this.platform = null;
-    this.version = null;
-    this.uuid = null;
-    this.cordova = null;
-    this.model = null;
-    this.manufacturer = null;
-    this.isVirtual = null;
-    this.serial = null;
-
-    var me = this;
-
-    channel.onCordovaReady.subscribe(function() {
-        me.getInfo(function(info) {
-            //ignoring info.cordova returning from native, we should use value from cordova.version defined in cordova.js
-            //TODO: CB-5105 native implementations should not return info.cordova
-            var buildLabel = cordova.version;
-            me.available = true;
-            me.platform = info.platform;
-            me.version = info.version;
-            me.uuid = info.uuid;
-            me.cordova = buildLabel;
-            me.model = info.model;
-            me.isVirtual = info.isVirtual;
-            me.manufacturer = info.manufacturer || 'unknown';
-            me.serial = info.serial || 'unknown';
-            channel.onCordovaInfoReady.fire();
-        },function(e) {
-            me.available = false;
-            utils.alert("[ERROR] Error initializing Cordova: " + e);
-        });
-    });
-}
-
-/**
- * Get device info
- *
- * @param {Function} successCallback The function to call when the heading data is available
- * @param {Function} errorCallback The function to call when there is an error getting the heading data. (OPTIONAL)
- */
-Device.prototype.getInfo = function(successCallback, errorCallback) {
-    argscheck.checkArgs('fF', 'Device.getInfo', arguments);
-    cordova.exec(function(params){ console.log("done it"); }, function(error){ console.log("got an error "+error); }, "Device", "getDeviceInfo", []);
-};
-
-    console.log(Device);
-
-
-
-device_id = Device.uuid;
+device_id = device.uuid;
 /*
 AdMob.prepareRewardVideoAd( {
 license: "lukas.nagel@gmx.ch/6af2fe6663be05e6b5e76d7afbb13ed8",
@@ -733,9 +673,6 @@ function _getLocalImagePathWithoutPrefix(url) {
 
 
 function getQuote(default_txt) {
-    cordova.exec(function(params){ console.log("done it"); }, function(error){ console.log("got an error "+error); }, "Device", "getDeviceInfo", []);
-
-console.log(Device.cordova);
 var author = randomKey(quote_array);
 var quote = quote_array[author];
 
